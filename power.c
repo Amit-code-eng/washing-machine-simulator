fe358bb2e9b4af451261a1e6ec315c853c635771
Amit-code-eng <amitdilipjaiswal191220@gmail.com>
2026-08-22 21:47:33 +0530

    v1

--- power.c
+++ power.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "power.h"
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+
+#define RESET "\033[0m"
 
 /*
  * Handle power failure while the machine is RUNNING.
@@ -14,8 +22,17 @@
 void power_failure(WashingMachine *machine)
 {
     /* TODO: Implement power failure logic */
+    if (machine->state == RUNNING)
+    {
+        // machine->door_status = DOOR_LOCKED;
+        machine->state = POWER_FAILURE;
+        printf(YELLOW "Power Failure Detected.\nRemaining time preserved: %d minutes.\n" RESET, machine->remaining_time);
+    }
+    else
+    {
+        printf(CYAN "No Active Washing Cycle.\n" RESET);
+    }
 }
-
 
 /*
  * Handle restoration of power.
@@ -30,4 +47,14 @@ void power_failure(WashingMachine *machine)
 void power_restore(WashingMachine *machine)
 {
     /* TODO: Implement power restoration logic */
+    if (machine->state == POWER_FAILURE)
+    {
+        machine->door_status = DOOR_CLOSED;
+        machine->state = RUNNING;
+        printf(GREEN "Power Restored.\nResuming Wash Cycle.\nRemaining Time: %d minutes.\n" RESET, machine->remaining_time);
+    }
+    else
+    {
+        printf(MAGENTA "Machine is Not in Power Failure State.\n" RESET);
+    }
 }
--- timer.c
+++ timer.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define GREEN "\033[32m"
+
+#define RESET "\033[0m"
+
 /*
  * Return the wash duration for the selected mode:
  * Heavy  -> 45 minutes
@@ -11,8 +15,11 @@
 int get_mode_duration(WashMode mode)
 {
     /* TODO: Implement mode duration logic */
-}
 
+    return (mode == MODE_HEAVY) ? 45 : (mode == MODE_NORMAL) ? 30
+                                   : (mode == MODE_LIGHT)    ? 20
+                                                             : 0;
+}
 
 /*
  * Timer operates only while the machine is RUNNING.
@@ -23,8 +30,19 @@ int get_mode_duration(WashMode mode)
 void timer_tick(WashingMachine *machine)
 {
     /* TODO: Implement timer logic */
-}
+    if (machine->state != RUNNING)
+        return;
 
+    else if (machine->remaining_time)
+    {
+        machine->remaining_time--;
+    }
+    else
+    {
+        machine->state = IDLE;
+        printf(GREEN "\nWashing Cycle Completed.\n" RESET);
+    }
+}
 
 /*
  * Background timer thread.
@@ -36,4 +54,13 @@ void timer_tick(WashingMachine *machine)
 void *timer_thread(void *arg)
 {
     /* TODO: Implement background timer logic */
+    WashingMachine *machine = (WashingMachine *)arg;
+
+    while (1)
+    {
+        sleep(1);
+        timer_tick(machine);
+    }
+
+    return NULL;
 }
